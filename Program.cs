23a60f6b5813234f85b527da41b5cd496907c88b
Vache <vache.khachatryan1998@gmail.com>
2021-08-10 12:02:17 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+        static void ChangeEntryPoint()
+        {
+            Console.WriteLine("The entry point is changed!");
+            Console.ReadLine();
+        }
+    }
+}
